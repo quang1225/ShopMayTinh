93f96757bc574943627cec8eeabc1bd6c4120825
phuong270594 <phuong2705941@gmail.com>
2017-01-08 23:11:20 +0700

    Nguyen quôc Quân(ql đơn hang + ql tai khoang)

---
+++ QLDONHANG + QLTAIKHOAN/QLDONHANG + QLTAIKHOAN/Models/Giohang.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopMayTinh.Models
+{
+    public class Giohang
+    {
+        //Tạo đối tượng chứa dữ liệu
+        dbShopMayTinhDataContext data = new dbShopMayTinhDataContext();
+
+        public int sMaSP { set; get; }
+        public string sTenSP { set; get; }
+        public string sHinhAnh { set; get; }
+        public Double sDonGia { set; get; }
+        public int sSoLuong { set; get; }
+        public Double sThanhTien
+        {
+            get { return (sSoLuong * sDonGia); }
+        }
+
+        //Khởi tạo giỏ hàng theo MaSP với số lượng mặc định là 1
+        public Giohang( int MaSP)
+        {
+            sMaSP = MaSP;
+            SANPHAM sp = data.SANPHAMs.Single( a => a.MaSP == sMaSP);
+            sTenSP = sp.TenSP;
+            sHinhAnh = sp.HinhAnh;
+            sDonGia = double.Parse(sp.DonGia.ToString());
+            sSoLuong = 1;
+        }
+
+
+    }
+}
